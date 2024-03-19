e60585791976bf31b95d609cbde6ba7b5d9b1ffc
ZukkoDev <tomyyou12@gmail.com>
2024-03-19 01:41:00 -0300

    cambio menor

--- En Vivo - Henry/Clase 11 - Mongoose/demo-moongose/index.js
+++ En Vivo - Henry/Clase 11 - Mongoose/demo-moongose/index.js
@@ -1,7 +1,7 @@
 const moongose = require("moongose");
 
 const URI =
-  "mongodb+srv://tomyrios2006:ze9UiSmcPlgJ5SuD@cluster0.n3gsi6w.mongodb.net/";
+  "";
 
 const dbConecction = async () => {
   await moongose.connect(URI);
